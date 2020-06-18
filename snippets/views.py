1c993e66fd5e6e1836defea703815af0c568510f
Danilo Moura <danilolmoura@gmail.com>
2020-06-18 20:48:14 -0300

    Add hyperlink to the API

--- snippets/views.py
+++ snippets/views.py
@@ -2,7 +2,10 @@ from rest_framework import mixins
 from rest_framework import generics
 from rest_framework import permissions
 from rest_framework import status
+from rest_framework import renderers
+from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 
 from django.http import Http404
@@ -13,6 +16,7 @@ from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 
 from .permissions import IsOwnerOrReadOnly
+
 
 class SnippetList(
     mixins.ListModelMixin,
@@ -34,6 +38,7 @@ class SnippetList(
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
 
 class SnippetDetail(
     mixins.RetrieveModelMixin,
@@ -66,3 +71,20 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
